b3869a473a1eb3007fb5e1399603b0b05ee1ea3a
Braden Wittkop <bradenjwittkop@gmail.com>
2023-07-31 18:27:55 -0500

    added two debugging questions

---
+++ debugging/debugging.js
@@ -0,0 +1,29 @@
+// Grasshopper - Function syntax debugging
+// A student was working on a function and made some syntax mistakes while coding. Help them find their mistakes and fix them.
+
+// function main [verb, noun]
+// return verb + noun
+// }
+
+// const main = (verb, noun) => verb + noun;
+
+
+// Debugging sayHello function
+// The starship Enterprise has run into some problem when creating a program to greet everyone as they come aboard. It is your job to fix the code and get the program working again!
+//
+//     Example output:
+//
+//     Hello, Mr. Spock
+
+// function sayHello(name) {
+//     return 'Hello'
+// }
+
+const sayHello = (name) => {
+    return `Hello, ${name}`
+}
+
+// or
+
+const sayHelloPartTwo = name => `Hello, ${name}`;
+
