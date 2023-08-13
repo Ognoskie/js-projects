7a94fa548b44ef924646331affb291dc3d884837
Braden Wittkop <bradenjwittkop@gmail.com>
2023-08-13 17:36:38 -0500

    added html from site

---
+++ front-end-mentor/js/news-letter.js
@@ -0,0 +1,17 @@
+// 📝Brief
+// Your challenge is to build out this newsletter form and get it looking as close to the design as possible.
+//
+//     You can use any tools you like to help you complete the challenge. So if you've got something you'd like to practice, feel free to give it a go.
+//
+//     Your users should be able to:
+//
+// 1. Add their email and submit the form
+// 2. See a success message with their email after successfully submitting the form
+// 3. See form validation messages if:
+// 4. The field is left empty
+// 5. The email address is not formatted correctly
+// 6. View the optimal layout for the interface depending on their device's screen size
+// 7. See hover and focus states for all interactive elements on the page
+// 8. Download the project and go through the README.md file. This will provide further details about the project and help you get set up.
+
+
