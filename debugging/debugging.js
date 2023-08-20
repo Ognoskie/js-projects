b87e8c3fc61a53d260816e21f27216ee17d1f6b7
Braden Wittkop <bradenjwittkop@gmail.com>
2023-08-19 21:00:47 -0500

    added skeleton to problem

--- debugging/debugging.js
+++ debugging/debugging.js
@@ -290,3 +290,23 @@
 // "abc"     -->  "Alpha Bravo Charlie"
 // "babble"  -->  "Bravo Alpha Bravo Bravo Lima Echo"
 // "Banana"  -->  "Bravo Alpha November Alpha November Alpha"
+
+
+
+var nato = (function() {
+    var letters =  {
+        "A": "Alpha",  "B": "Bravo",   "C": "Charlie",
+        "D": "Delta",  "E": "Echo",    "F": "Foxtrot",
+        "G": "Golf",   "H": "Hotel",   "I": "India",
+        "J": "Juliett","K": "Kilo",    "L": "Lima",
+        "M": "Mike",   "N": "November","O": "Oscar",
+        "P": "Papa",   "Q": "Quebec",  "R": "Romeo",
+        "S": "Sierra", "T": "Tango",   "U": "Uniform",
+        "V": "Victor", "W": "Whiskey", "X": "X-ray",
+        "Y": "Yankee", "Z": "Zulu"
+    }
+
+    return function(word) {
+
+    }
+})()
