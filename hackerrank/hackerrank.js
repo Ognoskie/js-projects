86db2275609b5445dd21bc648e5ca6523eca4705
Braden Wittkop <bradenjwittkop@gmail.com>
2023-08-21 09:34:40 -0500

    solved hacker rank problem

--- hackerrank/hackerrank.js
+++ hackerrank/hackerrank.js
@@ -143,54 +143,116 @@
 // }
 
 
-///////////////////////////////////////// Time conversion /////////////////////////////////////////
+///////////////////////////////////////// A Very Big Sum /////////////////////////////////////////
 
-// Given a time in -hour AM/PM format, convert it to military (24-hour) time.
-//
-//     Note: - 12:00:00AM on a 12-hour clock is 00:00:00 on a 24-hour clock.
-// - 12:00:00PM on a 12-hour clock is 12:00:00 on a 24-hour clock.
-//
-//     Example
-//
-//
-// Return '12:01:00'.
-//
-//
-//     Return '00:01:00'.
+// In this challenge, you are required to calculate and print the sum of the elements in an array, keeping in mind that some of those integers may be quite large.
 //
 //     Function Description
 //
-// Complete the timeConversion function in the editor below. It should return a new string representing the input time in 24 hour format.
+// Complete the aVeryBigSum function in the editor below. It must return the sum of all array elements.
 //
-//     timeConversion has the following parameter(s):
+//     aVeryBigSum has the following parameter(s):
 //
-// string s: a time in  hour format
-// Returns
+// int ar[n]: an array of integers .
+//     Return
 //
-// string: the time in  hour format
+// long: the sum of all array elements
 // Input Format
 //
-// A single string  that represents a time in -hour clock format (i.e.:  or ).
+// The first line of the input consists of an integer .
+//     The next line contains  space-separated integers contained in the array.
 //
-// Constraints
+//     Output Format
 //
-// All input times are valid
+// Return the integer sum of the elements in the array.
+//
+//     Constraints
+//
+//
+// Sample Input
+//
+// 5
+// 1000000001 1000000002 1000000003 1000000004 1000000005
+// Output
+//
+// 5000000015
+// Note:
+//
+//     The range of the 32-bit integer is .
+//     When we add several integer values, the resulting sum might exceed the above range. You might need to use long int C/C++/Java to store such sums.
 
 
-function timeConversion(s) {
-
-
-}
-
-
+// const aVeryBigSum = (ar) => {
+//     return eval(ar.join("+"));
+// }
 
 
 
 
 
 
+//////////////////////////////////////////////////////// Diagonal Difference /////////////////////////////////////////////
+
+// Given a square matrix, calculate the absolute difference between the sums of its diagonals.
+//
+// For example, the square matrix  is shown below:
+//
+// 1 2 3
+// 4 5 6
+// 9 8 9
+// The left-to-right diagonal = . The right to left diagonal = . Their absolute difference is .
+//
+//     Function description
+//
+// Complete the  function in the editor below.
+//
+//     diagonalDifference takes the following parameter:
+//
+//     int arr[n][m]: an array of integers
+// Return
+//
+// int: the absolute diagonal difference
+// Input Format
+//
+// The first line contains a single integer, , the number of rows and columns in the square matrix .
+//     Each of the next  lines describes a row, , and consists of  space-separated integers .
+//
+//     Constraints
+//
+// Output Format
+//
+// Return the absolute difference between the sums of the matrix's two diagonals as a single integer.
+//
+// Sample Input
+//
+// 3
+// 11 2 4
+// 4 5 6
+// 10 8 -12
+// Sample Output
+//
+// 15
+// Explanation
+//
+// The primary diagonal is:
+//
+//     11
+// 5
+// -12
+// Sum across the primary diagonal: 11 + 5 - 12 = 4
+//
+// The secondary diagonal is:
+//
+//     4
+// 5
+// 10
+// Sum across the secondary diagonal: 4 + 5 + 10 = 19
+// Difference: |4 - 19| = 15
+//
+// Note: |x| is the absolute value of x
 
 
+/////// code below
 
 
 
