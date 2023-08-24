e5adba1ea2e9f9eb1ec3893ef74a82908a0ce7bd
Braden Wittkop <bradenjwittkop@gmail.com>
2023-08-24 09:24:12 -0500

    added html for new dom problem

--- dom/dom.js
+++ dom/dom.js
@@ -3,7 +3,7 @@ const getElementByNamedItem = document.getElementsByTagName('div').namedItem('tw
 const boldText = document.querySelector('.bear');
 const buttonElem = document.querySelector("#wrapper button");
 const inputElem = document.querySelector("#wrapper input");
-
+const listItems = document.querySelectorAll('#list li');
 
 
 
@@ -29,7 +29,9 @@ const buttonElmFunction = () => {
     const oldText = inputElem.value;
     return inputElem.value = oldText === "ON" ? "OFF" : "ON";
 }
-
+const handleHover = (e) => {
+    return e.target.innerText = 'ON'
+};
 
 
 
@@ -50,6 +52,11 @@ const buttonElmFunction = () => {
 getElementByNamedItem.addEventListener('click', changingTwosColor);
 boldText.addEventListener('dblclick', boldLoremText);
 buttonElem.addEventListener('click', buttonElmFunction)
+if(listItems.length > 1) {
+    listItems.forEach(item => item.addEventListener('mouseover', handleHover));
+}
+
+
 
 
 
