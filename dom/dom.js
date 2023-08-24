26da25063a0a7188041c523d03e89dfeaefb7c48
Braden Wittkop <bradenjwittkop@gmail.com>
2023-08-24 09:30:19 -0500

    added and finished two dom problems

--- dom/dom.js
+++ dom/dom.js
@@ -4,8 +4,9 @@ const boldText = document.querySelector('.bear');
 const buttonElem = document.querySelector("#wrapper button");
 const inputElem = document.querySelector("#wrapper input");
 const listItems = document.querySelectorAll('#list li');
-
-
+const button = document.getElementById('button');
+const input = document.getElementById('input');
+const element = document.getElementById('element');
 
 
 
@@ -21,18 +22,23 @@ const listItems = document.querySelectorAll('#list li');
 ///////////////////////////////////////////// Functions /////////////////////////////////////////////////
 const changingTwosColor = () => {
     getElementByNamedItem.style.color = 'blue';
-}
+};
 const boldLoremText = () => {
     boldText.style.fontWeight = 'bold';
-}
+};
 const buttonElmFunction = () => {
     const oldText = inputElem.value;
     return inputElem.value = oldText === "ON" ? "OFF" : "ON";
-}
+};
 const handleHover = (e) => {
     return e.target.innerText = 'ON'
 };
-
+const handleClick = () => {
+    input.value = 'Hello World';
+};
+const changeText = () => {
+    element.innerText = 'Thanks!';
+};
 
 
 
@@ -51,11 +57,12 @@ const handleHover = (e) => {
 ///////////////////////////////////////// Event listeners /////////////////////////////////////////////////
 getElementByNamedItem.addEventListener('click', changingTwosColor);
 boldText.addEventListener('dblclick', boldLoremText);
-buttonElem.addEventListener('click', buttonElmFunction)
+buttonElem.addEventListener('click', buttonElmFunction);
 if(listItems.length > 1) {
     listItems.forEach(item => item.addEventListener('mouseover', handleHover));
-}
-
+};
+button.addEventListener('click', handleClick);
+element.addEventListener("mouseover", changeText);
 
 
 
