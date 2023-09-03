b72fd3e6c7d2069260811f1680c1339780b0e4de
Braden Wittkop <bradenjwittkop@gmail.com>
2023-09-03 12:35:20 -0500

    added heb internship question

--- debugging/debugging.js
+++ debugging/debugging.js
@@ -363,26 +363,41 @@ function parameter(n) {
 
 
 
+//////////////////// HEB Internship //////////////////////////////////
 
-////////// js notes
+// Happy Numbers
+// Programming challenge description:
+//     A happy number is defined by the following process. Starting with any positive integer, replace the number by the sum of the squares of its digits, and repeat the process until the number equals 1 (where it will stay), or it loops endlessly in a cycle which does not include 1. Those numbers for which this process ends in 1 are happy numbers, while those that do not end in 1 are unhappy numbers.
+//     Input:
+// Your program should read lines of text from standard input. Each line contains a single positive integer, N.
+//     Output:
+// If the number is a happy number, print 1 to standard output. Otherwise, print 0.
+//
+// For the curious, here's why 7 is a happy number: 7->49->97->130->10->1. Here's why 22 is NOT a happy number: 22->8->64->52->29->85->89->145->42->20->4->16->37->58->89 ...
+// Test 1
+// Test Input
+// Download Test 1 Input
+// 1
+// Expected Output
+// Download Test 1 Output
+// 1
+// Test 2
+// Test Input
+// Download Test 2 Input
+// 7
+// Expected Output
+// Download Test 2 Output
+// 1
+// Test 3
+// Test Input
+// Download Test 3 Input
+// 22
+// Expected Output
+// Download Test 3 Output
+// 0
 
-// addTwo.js module:
-function addTwo(a, b) {
-    console.log(a + b);
-}
-
-export default addTwo;
 
 
-
-import addTwo from "./addTwo";
-
-// the rest of the mathOperations.js code goes here
-
-
-import { addTwo } from "./addTwo";
-
-// the rest of the mathOperations.js code goes here
 
 
 
