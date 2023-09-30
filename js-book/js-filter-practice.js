6662cacd175a8eae68684b7db751756b2352193f
Braden Wittkop <bradenjwittkop@gmail.com>
2023-09-30 12:03:23 -0500

    added note

--- js-book/js-filter-practice.js
+++ js-book/js-filter-practice.js
@@ -9,3 +9,12 @@ const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 console.log(numbers)
 const evenNumbers = filterEvenNumbers(numbers);
 console.log(evenNumbers); // Should print [2, 4, 6, 8, 10]
+
+
+const searchActivityByLocation = (activities, searchLocation) => {
+    return activities.filter(activity => {
+        return activity.location.toLowerCase().includes(searchLocation.toLowerCase());
+    })
+}
+
+// ask steve why do i have to return line 16 when i am returning line 15 already and line 16 is within line 15 brackets
